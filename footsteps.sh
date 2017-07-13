8fee8720155fb485e397970c40ab4ca39cc9962d
Jens-Peter Brodersen <jpb@Jens-Peters-MacBook-Pro-2.local>
2017-07-13 19:16:57 +0200

    footsteps completed

--- footsteps.sh
+++ footsteps.sh
@@ -7,16 +7,15 @@ footsteps=$DIR1/Slime-universe/themelibrary/footsteps
 materials=$DIR1/Slime-universe/baselibrary/materials
 targetdir=$DIR1/Slime-universe/Slime-universe/footsteps
 
-# DIR=$materials
 IFS='
 '
-speedmin=-300
-speedmax=500
+speedmin=-200
+speedmax=200
 
 
 echo "materials location: $materials"
 echo "footsteps location: $footsteps"
-echo "targetdir location: $targetdir"
+echo "targetdir location: $targetdir $IFS"
 
 
 # Get number of files in material source folder
@@ -25,7 +24,7 @@ if [[ -d "${materials}" ]]
     then
         materialfile_matrix=($(ls "${materials}"))
         num_materialfiles=${#materialfile_matrix[*]}
-
+        # echo "num_materialfiles = $num_materialfiles"
 fi
 
 # Get number of files in footsteps source folder
@@ -37,46 +36,56 @@ if [[ -d "${footsteps}" ]];
 
 fi
 
-echo "materials = $num_materialfiles"
-echo "footsteps = $num_footstepfiles"
+# echo "materials = $num_materialfiles"
+# echo "footsteps = $num_footstepfiles"
+
+
+# Debug arrays
+# echo "Array items and indexes:"
+# for index in ${!materialfile_matrix[*]}
+# do
+#     printf "%4d: %s\n" $index ${materialfile_matrix[$index]}
+# done
+
 
 # Loop though each footstep and mix with each material 
-
 # First the Footsteps
-for footcount in `seq 0 $num_footstepfiles`;
+for footcount in `seq 0 $(($num_footstepfiles-1))`;
     do
         currentfootstep=$footsteps/${footstepfile_matrix[$footcount]}
-        footfilename=${footstepfile_matrix[$footcount]}
+        footfile=${footstepfile_matrix[$footcount]}
 
         # echo "currentfootstep = $currentfootstep"
-        # echo "filename = $footfilename"
+        # echo "footstep-filename = $footfile $IFS"
 
         # Then loop through each material
-        for materialcount in `seq 0 $num_materialfiles`;
+        for materialcount in `seq 0 $(($num_materialfiles-1))`;
             do
+                # echo "materialcount = $materialcount"
                 currentmaterial=$materials/${materialfile_matrix[$materialcount]}
-                materialfilename=${materialfile_matrix[$materialcount]}
-                materialname=$(echo $materialfilename | cut -f 1 -d '.');
+                materialfile=${materialfile_matrix[$materialcount]}
+                materialname=$(echo $materialfile | cut -f 1 -d '.');
         
                 # echo "currentmaterial = $currentmaterial"
-                # echo "filename = $materialfilename"
+                # echo "material-filename = $materialfile"
 
-                # ...and mix them together
-                echo "Creating footstep: $footfilename + $materialfilename"
+                # ...and then mix them together                
+                echo "Creating footstep $materialcount: $footfile ($speedvalue) + $materialfile"
 
-                sox -M $currentfootstep -v0.2 $currentmaterial $targetdir/footstep_0$((footcount+1))_$materialname.wav remix -
+                # Add random pitch to footsteps
+                speedvalue=$((RANDOM % ($speedmax-$speedmin) + $speedmin))
+                sox -V1 $currentfootstep currentfootstep_temp.wav speed $speedvalue"c" 
+
+                # Add fadein on materials
+                sox -V1 $currentmaterial currentmaterial_temp.wav fade t 0.1 0
+
+                # Mix footstep and material together
+                sox -V1 -M currentfootstep_temp.wav -v0.9 currentmaterial_temp.wav $targetdir/footstep_0$((footcount+1))_$materialname.wav remix -
+
 
             done
 
-
-
-
-
-
-
-
-
-    
+            echo "$IFS""Footsteps completed...$IFS"
     done
 
 
